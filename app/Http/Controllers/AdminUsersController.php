6e293a9500f521a60da9dc3806840111d58d68ed
Character XY <webpsyurl@gmail.com>
2019-04-26 11:11:52 +0200

    added user create

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +19,10 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +32,10 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+        
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -34,9 +44,15 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+         $user = User::create($request->all());
+
+         return redirect('/admin/users');
+       
+       
+       
+        //return $request->all();
     }
 
     /**
@@ -47,7 +63,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        //
+        return  view('admin.users.show');
     }
 
     /**
